e19ca6c53e466e4d899931ef099661cf0c9c8ad7
LLLzxqh <Yuhang.Lin@student.uts.edu.au>
2022-05-04 15:30:18 +0800

    Tutorial off

--- Assets/Scenes/Scripts/Level1move.cs
+++ Assets/Scenes/Scripts/Level1move.cs
@@ -1,23 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Level1move : MonoBehaviour
 {
     public float moveSpeed;
     public GameObject text;
     private float ver, hor;
-    Camera camera1;
-
+  
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerPrefs.SetFloat("number", 0);
-        camera1 = Camera.main;
-        //PlayerPosX = PlayerPrefs.GetFloat("");
     }
 
     // Update is called once per frame
@@ -29,10 +24,12 @@ public class Level1move : MonoBehaviour
         transform.position += transform.up * ver * moveSpeed * Time.deltaTime;
         transform.position += transform.right * hor * moveSpeed * Time.deltaTime;
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    Debug.Log(PlayerPrefs.GetFloat("number"));
-        //}
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(PlayerPrefs.GetFloat("number"));
+        }
 
         if (PlayerPrefs.GetFloat("number") >= 1)
         {
@@ -61,34 +58,12 @@ public class Level1move : MonoBehaviour
             GameObject.Find("Door4").gameObject.GetComponent<Renderer>().enabled = false;
         }
 
-        Vector3 playerPos = GameObject.Find("player").transform.position;
-        //float PlayX = GameObject.Find("player").transform.position.x;
-        //float PlayY = GameObject.Find("player").transform.position.y;
-
-        float x = camera1.transform.position.x;
-
-        if (x < 40)
-        {
-            if (Input.GetKeyDown(KeyCode.J))
-            {
-                Vector3 PlayerPos = GameObject.Find("player").transform.position;
-                camera1.transform.position = new Vector3(55f, 0f, -10f);
-                GameObject.Find("player").transform.position = new Vector3(44f,8f,0f);
-            }
-        }
-        if (x >= 40)
-        {
-            if (Input.GetKeyDown(KeyCode.K))
-            {
-                camera1.transform.position = new Vector3(0f, 0f, -10f);
-            }
-        }
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //Debug.Log(collision.name);
+        Debug.Log(collision.name);
         if (collision.name == "monster")
         {
             Destroy(GameObject.Find("monster"), 0.5f);
@@ -97,7 +72,6 @@ public class Level1move : MonoBehaviour
 
         if (collision.name == "End")
         {
-            Time.timeScale = 0;
             text.SetActive(true);
         }
     }
--- Assets/Scenes/Scripts/MonsterMove.cs
+++ Assets/Scenes/Scripts/MonsterMove.cs
@@ -19,7 +19,6 @@ public class MonsterMove : MonoBehaviour
     void Update()
     {
         Moving();
-
     }
 
     void Moving()
